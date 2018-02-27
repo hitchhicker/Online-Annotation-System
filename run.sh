849d91ee38e5da4cc2625f011adf4ff9245b3fcf
hitchhicker <yubokai8@gmail.com>
2018-02-27 23:36:48 +0100

    run multiples parameter for docker

--- run.sh
+++ run.sh
@@ -1,1 +1,20 @@
-docker run -d -e XML_SAVE_PATH=/home/ -e IMAGE_SAVE_PATH=/home -v $(pwd)/:/var/www/app --net=host -ti annotation
+if [ $1 == "-s" ]
+    # server
+then
+    docker run -d -e XML_SAVE_PATH=. -e IMAGE_SAVE_PATH=. -v $(pwd)/:/var/www/app --net=host -ti annotation
+elif [ $1 == "-d" ]
+    # debug
+then
+    docker run --rm -e XML_SAVE_PATH=. -e IMAGE_SAVE_PATH=. -v $(pwd)/:/var/www/app --net=host -ti annotation /bin/bash
+elif [ $1 == "-r" ]
+then
+    # remove
+    docker rm -f $(docker ps -a -q)
+elif [ $1 == "-h" ]
+then
+    echo '-s: [server] lauch server'
+    echo '-d: [debug]  enter docker container'
+    echo '-r: [remove] remove all containers'
+else
+    "Unknown argument run.sh -h to see more."
+fi
